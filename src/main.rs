b3656c442204d9ee0de289c77515eaee7cd7aee8
fabien.giannesini <fabien.giannesini@gmail.com>
2021-03-18 17:14:06 +0100

    Should trigger player 1 points

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,22 @@ struct Scoreboard {
 impl Scoreboard {
     fn score(&self) -> String {
         let mut score = "".to_string();
-        if self.player1 == 0 {
-            score.push_str("Love")
+        match self.player1 {
+            0 => score.push_str("Love"),
+            1 => score.push_str("Fifteen"),
+            2 => score.push_str("Thirty"),
+            3 => score.push_str("Forty"),
+            _ => panic!()
         }
         score.push_str(" ");
         if self.player2 == 0 {
             score.push_str("Love")
         }
         score
+    }
+
+    fn trigger_player1(&mut self) {
+        self.player1 = self.player1 + 1;
     }
 }
 
@@ -29,5 +37,29 @@ mod tests {
     fn should_init_scoreboard() {
         let scoreboard: Scoreboard = Default::default();
         assert_eq!(scoreboard.score(), "Love Love")
+    }
+
+    #[test]
+    fn should_display_fifteen_if_player_one_win_one_time() {
+        let mut scoreboard: Scoreboard = Default::default();
+        scoreboard.trigger_player1();
+        assert_eq!(scoreboard.score(), "Fifteen Love")
+    }
+
+    #[test]
+    fn should_display_thirty_if_player_one_win_two_times() {
+        let mut scoreboard: Scoreboard = Default::default();
+        scoreboard.trigger_player1();
+        scoreboard.trigger_player1();
+        assert_eq!(scoreboard.score(), "Thirty Love")
+    }
+
+    #[test]
+    fn should_display_forty_if_player_one_win_three_times() {
+        let mut scoreboard: Scoreboard = Default::default();
+        scoreboard.trigger_player1();
+        scoreboard.trigger_player1();
+        scoreboard.trigger_player1();
+        assert_eq!(scoreboard.score(), "Forty Love")
     }
 }
